7508214aaa11605bf8badeac85f1bc726fd9ca24
xiangb <xiangadrien@hotmail.com>
2016-11-09 22:35:21 +0100

    define par_to_vec

--- KB/par2vec.py
+++ KB/par2vec.py
@@ -1,6 +1,33 @@
-#TO COMPLETE !!!!!
-import numpy as np
+# -*- coding: utf-8 -*-
 
-def getVec(par):
-	vec = np.zeros((100,))
-	return vec
+#TO COMPLETE !!!!!
+
+import numpy as np
+from gensim.models.doc2vec import Doc2Vec 
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+
+
+# Loading of trained model ( Wikipedia ), to be charged one time 
+model_doc2vec = Doc2Vec.load('./enwiki_dbow/enwiki_dbow/doc2vec.bin')
+stopwds = stopword.words('english')
+
+def par_to_vec(paragraph,stopwds,model_doc2vec):
+    """
+    Args :
+    paragraph : string to process, lower letter, remove punctuation, tokenize and remove stopwords
+    stopwds : list of stopwords to declare with stopwords.words
+    model_doc2vec : trained doc2vec model
+    
+    Output : 
+    
+    vect : vector representing the paragraph, size 300
+    """
+	
+    tok = RegexpTokenizer(r'\w+')
+    res_inter = tok.tokenize(paragraph.lower())
+    res_inter_stop = [w for w in res_inter if w not in stopwords]
+    vect = model_doc2vec.infer_vector(output)
+    return vect
+
+
